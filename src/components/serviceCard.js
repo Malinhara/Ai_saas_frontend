19ed67ddcc91812b2f4221d5878ba6377b6fbda3
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-24 22:55:37 +0530

    Update serviceCard.js

--- src/components/serviceCard.js
+++ src/components/serviceCard.js
@@ -1,7 +1,9 @@
 export default function Card() {
     return (
       <>
-        <div className="overflow-hidden bg-gray-50 py-24 sm:py-32">
+        <div className="overflow-hidden py-24 sm:py-32" style={{
+    background: "linear-gradient(to right, white, #b3d7f8 50%, white)",
+  }}>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
               {/* Left Section */}
