ba77452d68c92d58370206c2a531b734cbe16748
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-25 09:18:45 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -27,14 +27,14 @@ export default function Header() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Image AI King</span>
+           <span className="self-center text-sm md:text-lg font-semibold whitespace-nowrap dark:text-white">Image AI King</span>
           </a>
 
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
   onClick={user.isLoggedIn ? logout : toggleDropdown} // Log out if logged in, otherwise toggle dropdown
   type="button"
-  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
   {user.isLoggedIn ? "Log Out" : "Get Started"}
 </button>
 
@@ -48,14 +48,14 @@ export default function Header() {
   <li>
     <Link
       to="/signin"
-      className="block px-2 py-2 text-white hover:bg-slate-500">
+      className="block px-2 py-2 text-black hover:bg-gray-100">
       Sign In
     </Link>
   </li>
   <li>
     <Link
       to="/signup"
-      className="block px-2 py-2 text-white hover:bg-slate-500"
+      className="block px-2 py-2 text-black hover:bg-gray-100"
     >
       Sign Up
     </Link>
