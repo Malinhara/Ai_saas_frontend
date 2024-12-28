9491592f50b5a5295bdffbfc958395c3ea97d932
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-28 23:27:35 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,7 +25,7 @@ export default function Home() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
 
-                <a href="#"  className="rounded-lg bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <a href="/talkimage"  className="rounded-lg bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Create Free Talk Image<span aria-hidden="true"> →</span>
                 </a>
               </div>
