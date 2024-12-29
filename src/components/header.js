2542951a38e595c05551a9ee038fde00c3008bc2
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-29 23:25:29 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,7 @@ export default function Header() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-           <span className="self-center text-sm md:text-lg font-semibold whitespace-nowrap dark:text-white">Image AI King</span>
+           <span className="self-center text-sm md:text-lg font-semibold whitespace-nowrap dark:text-white">VISUALS</span>
           </a>
 
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
