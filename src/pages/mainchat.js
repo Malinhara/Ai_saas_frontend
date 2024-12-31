dbf9232dea8dc3899c02eeba2bbe04dcf50b86b0
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-12-31 20:28:03 +0530

    Add files via upload

---
+++ src/pages/mainchat.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Chat from '../components/chat';
+import Sidebar from '../components/sidebar';
+import '../styles/style.css';
+
+function Mainchat() {
+
+
+  return (
+    <div className="app-container">
+      <Sidebar/>
+      <Chat/>
+    </div>
+  );
+}
+
+export default Mainchat;
