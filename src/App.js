d69f3f456f5633a7f3e3520729c2ae57b342e928
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2025-01-10 22:22:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // Import necessary components
+import AgentManagement from "./components/Agentmangment";
+import AIMarketplace from "./components/AIMarketplace";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import BotConfig from "./pages/botConfigure";
+import ContactPage from "./pages/contact";
 import CreateAvatar from "./pages/createAvatar";
 import Home from "./pages/home";
+import Mainchat from "./pages/mainchat";
 import ServicesPage from "./pages/services";
 import Signin from "./pages/signin";
 import Signup from "./pages/signup";
 import Talkimage from "./pages/talkimage";
-import ContactPage from "./pages/contact";
-import Mainchat from "./pages/mainchat";
-import BotConfig from "./pages/botConfigure";
-import AgentManagement from "./components/AgentManagment";
+import PrivateRoute from "./Logichandle/Auth";
 
 function App() {
   return (
@@ -24,9 +26,17 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/service" element={<ServicesPage/>}/>
         <Route path="/contact" element={<ContactPage/>}/>
-        <Route path="/chat" element={<Mainchat/>}/>
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Mainchat />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/agentmanagment" element={<AgentManagement/>}/>
         <Route path="/botconfigure" element={<BotConfig/>}/>
-        <Route path="/agentmnagment" element={<AgentManagement/>}/>
+        <Route path="/aImarketplace" element={<AIMarketplace/>}/>
       </Routes>
       <Footer />
     </Router>
