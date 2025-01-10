586457ace7e654827422bc9fa589e50e82160ad7
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2025-01-10 23:59:09 +0530

    Update AIMarketplace.js

--- src/components/AIMarketplace.js
+++ src/components/AIMarketplace.js
@@ -84,7 +84,7 @@ const AIMarketplace = () => {
              Close
            </button>
            <Link
-             to="/botconfigure"
+             to="/agentmanagment"
              className="px-4 py-2 text-sm text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300"
            >
              Create Agent
