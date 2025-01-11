f758615af44093384ac326499b4797cd0ad40c3f
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2025-01-11 13:34:03 +0530

    Update chatProfile.js

--- src/components/chatProfile.js
+++ src/components/chatProfile.js
@@ -12,6 +12,7 @@ const fetchUserData = async () => {
         const data = response.data;
 
         if (data.success) {
+            console.log(data)
             localStorage.setItem('agent',1)
             return data.data; // Return the fetched user data
         } else {
@@ -24,4 +25,25 @@ const fetchUserData = async () => {
     }
 };
 
-export { fetchUserData };
+
+const fetchAgentData = async () => {
+    try {
+        // Using GET method to fetch the agent data
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/user/agents`); // Replace this with your actual endpoint for agents
+
+        const data = response.data;
+
+        if (data.success) {
+            return data.data; // Return the fetched agent data
+        } else {
+            console.error(data.message);
+            return []; // Return empty array on failure
+        }
+    } catch (error) {
+        console.error('Error fetching agent data:', error);
+        return []; // Return empty array on error
+    }
+};
+
+export { fetchUserData,fetchAgentData  };
+
